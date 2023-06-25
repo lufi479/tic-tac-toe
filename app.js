1d98b4fcbefc6a5af67009a26fb323952d4ccfaf
lufi479 <135998525+lufi479@users.noreply.github.com>
2023-06-25 15:14:09 -0700

    Add a button for a new game when a game ends and add all functionality for it

--- app.js
+++ app.js
@@ -70,29 +70,21 @@ const gameController = (() => {
         const board = gameBoard.getBoard();
         console.log(board);
         if ((board[0][0]!= "") && ((board[0][0] === board[1][1]) && (board[1][1] === board[2][2]))){
-            retVal = board[0][0];
             return board[0][0]
-            console.log('lol')
         }
         else if ((board[0][2]!= "") && ((board[0][2] === board[1][1]) && (board[1][1] === board[2][0]))){
-            retVal = board[0][2];
             return board[0][2];
-            console.log('0 2 -> 1 1 ->2 0')
         }
         else {
             for (let i = 0; i < 3; i++){
                 console.log(board[0][i] + " " + board[1][i] +" " + board[2][i]);
                 if ((board[0][i]!= "") && ((board[0][i] === board[1][i]) && (board[1][i] === board[2][i]))){
-                    retVal = board[0][i];
-                    return retVal;
-                    //break;
+                    return board[0][i]
                 }
                 else if ((board[i][0]!= "") && ((board[i][0] === board[i][1]) && (board[i][1] === board[i][2]))){
-                    retVal = board[i][0];
-                    return retVal;
-                    //break;
+                    return board[i][0]
                 }
-            }
+            }   
         }
 
     }
@@ -103,24 +95,22 @@ const gameController = (() => {
         let check = checkWinner();
         console.log(check);
         if (check === players[0].getMark()){
-            console.log(`${players[0].getName()} Wins!`);
             players[0].increaseWinCount();
-            //displayController.playerWin(players[0]);
+            switchActivePlayer();
             return players[0];
         }
         else if (check === players[1].getMark()){
-            console.log(`${players[1].getName()} Wins!`);
             players[1].increaseWinCount();
-            //displayController.playerWin(players[1]);
+            switchActivePlayer();
             return players[1];
         }
 
         if (turn === 8){
+            switchActivePlayer();
             return "tie"
         }
     
         switchActivePlayer();
-        console.log(turn);
         turn++;
     }
 
@@ -182,6 +172,7 @@ const displayController = (() => {
                 spots.item(i).disabled = true;
             }
         }
+        newGameDiv.style.display = "block";
         
     }
 
@@ -202,7 +193,6 @@ const displayController = (() => {
 
     function startGame(e){
         e.preventDefault();
-        console.log("lmao");
         let formData = new FormData(this);
         const playerOneName = formData.get("one-name");
         const playerTwoName = formData.get("two-name");
@@ -221,11 +211,21 @@ const displayController = (() => {
         gameController.resetTurns();
     }
 
+    function clickNewGame(e){
+        gameBoard.makeBoard();
+        updateDisplay();
+        gameController.resetTurns();
+        newGameDiv.style.display = "none";
+        updateDisplay();
+    }
+
     boardDiv.addEventListener("click", clickSpot);
 
     document.querySelector(".name-form").addEventListener("submit", startGame);
 
     restartDiv.addEventListener("click", clickRestart);
+
+    newGameDiv.addEventListener("click", clickNewGame);
 
     return {
         updateDisplay
